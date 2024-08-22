b03e6dd342c884b57c2e4e9d2529d4906f9c64be
maikisinovsky <maia.kisinovsky@gmail.com>
2024-08-22 15:49:25 -0300

    chore: ultimo avanzado

--- index.js
+++ index.js
@@ -4,16 +4,18 @@ let botonSubmit = document.getElementById('submit');
 let botonBorrar = document.getElementById('borrar');
 let botonMostrar = document.getElementById('mostrar');
 
-botonSubmit.addEventListener('click', function() {
-    cantidadJugadores = cantidadJugadores + 1; 
+document.getElementById('formulario').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    cantidadJugadores += 1;
     const nombre = document.getElementById('nombre').value;
     const numero = document.getElementById('numero').value;
     const posicion = document.getElementById('posicion').value;
 
     listaJugadores.push({ nombre, numero, posicion });
 
-    document.getElementById('formulario').reset(); 
-    
+    document.getElementById('formulario').reset();
+
     const ul = document.getElementById('listaJugadores');
     ul.innerHTML = '';
 
@@ -23,17 +25,19 @@ botonSubmit.addEventListener('click', function() {
         ul.appendChild(li);
     });
 
-    
-    if (cantidadJugadores > 26) {
+    if (cantidadJugadores >= 2) {
+        const mensaje = document.getElementById("mensaje");
         botonSubmit.disabled = true;
+        mensaje.innerHTML ="El máximo de jugadores ha sido alcanzado"
     }
 });
 
 botonBorrar.addEventListener('click', function() {
     listaJugadores = [];
-    cantidadJugadores = 0; 
+    cantidadJugadores = 0;
     const ul = document.getElementById('listaJugadores');
     ul.innerHTML = "";
-    
-    botonBorrar.botonSubmit = false;
+    mensaje.innerHTML ="";
+
+    botonSubmit.disabled = false;
 });
