bb8f023d24b710d61c2c94824c6cf4d336be64fb
maikisinovsky <maia.kisinovsky@gmail.com>
2024-09-26 10:17:34 -0300

    feat: manija wally terminado

--- TP2_Wally/index.js
+++ TP2_Wally/index.js
@@ -1,22 +1,37 @@
 import { generarEspacio } from "./busqueda.js";
 
-var encontrado = false;
-var lugar = -1;
 
-do {
-    let gen = generarEspacio(100);  
+var lugar = "Hola Lu :)";
 
-    console.log(gen)
 
-    for (let i = 0; i < gen.length; i++)  {
-        if (gen[i] === "Wally") {
+    let espacio = generarEspacio(100);  
+
+    console.log(espacio)
+
+    //manija 1
+    for (let i =  0; i < espacio.length; i++) {
+
+        if (espacio[i].startsWith('W')) {
+            console.log(`${espacio[i]} en la posición ${i} comienza con W`)
+        } 
+    }
+
+    //manija 2
+    for(let i= 0; i<espacio.length; i++){
+        if (espacio[i].includes('ll') || espacio[i].includes('Ll')) {
+            console.log(`${espacio[i]} en la posición ${i} tiene "ll"`)
+        }
+    }
+
+    //consigna base
+    for (let i = 0; i < espacio.length; i++)  {
+        if (espacio[i] === "Wally") {
             lugar = i;
             console.log("Wally está en la posición " + lugar);
-            encontrado = true; 
             break; 
         }
     
     }
-        
 
-} while (encontrado == false);
+
+
